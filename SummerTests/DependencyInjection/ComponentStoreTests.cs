93074dc87b4ff08c93cda05b720d0e66c8a78707
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-05-07 23:07:26 -0700

    Start working on ComponentStore tests

---
+++ SummerTests/DependencyInjection/ComponentStoreTests.cs
@@ -0,0 +1,74 @@
+﻿namespace SummerTests.DependencyInjection;
+
+public class ComponentStoreTests
+{
+    [Fact]
+    public void GenericFind_GivenAValidComponent_ShouldReturnSingleton()
+    {
+    }
+    
+    [Fact]
+    public void GenericFind_GivenAnInvalidComponent_ShouldReturnNull()
+    {
+    }
+    
+    [Fact]
+    public void Find_GivenAValidComponent_ShouldReturnSingleton()
+    {
+    }
+    
+    [Fact]
+    public void Find_GivenAnInvalidComponent_ShouldReturnNull()
+    {
+    }
+    
+    [Fact]
+    public void GenericRegister_GivenAValidComponent_ShouldNotThrow()
+    {
+    }
+    
+    [Fact] //TODO: Still in doubt about the behavior I want in this case.
+    public void GenericRegister_GivenAValidComponentThatAlreadyExists_ShouldNotThrow()
+    {
+    }
+    
+    [Fact]
+    public void GenericRegister_GivenAValidComponent_ShouldBeAbleToRetrieveItAfterwards()
+    {
+    }
+    
+    [Fact]
+    public void GenericRegister_GivenAnInvalidComponent_ShouldThrow()
+    {
+    }
+    
+    [Fact]
+    public void GenericRegister_GivenAnInvalidComponent_ShouldGetNullWhenRetrievingItAfterwards()
+    {
+    }
+    
+    [Fact]
+    public void Register_GivenAValidComponent_ShouldNotThrow()
+    {
+    }
+    
+    [Fact] //TODO: Still in doubt about the behavior I want in this case.
+    public void Register_GivenAValidComponentThatAlreadyExists_ShouldNotThrow()
+    {
+    }
+    
+    [Fact]
+    public void Register_GivenAValidComponent_ShouldBeAbleToRetrieveItAfterwards()
+    {
+    }
+    
+    [Fact]
+    public void Register_GivenAnInvalidComponent_ShouldThrow()
+    {
+    }
+    
+    [Fact]
+    public void Register_GivenAnInvalidComponent_ShouldGetNullWhenRetrievingItAfterwards()
+    {
+    }
+}
