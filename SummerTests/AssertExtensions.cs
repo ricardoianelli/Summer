d7a39038837e86e2b2611500a5a2afb28eeec2eb
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-05-14 21:50:32 -0700

    Create AssertExtensions.cs

---
+++ SummerTests/AssertExtensions.cs
@@ -0,0 +1,28 @@
+﻿namespace SummerTests;
+
+public static class AssertExtensions
+{
+    public static void DoesNotThrow(Action testCode)
+    {
+        try
+        {
+            testCode.Invoke();
+        }
+        catch (Exception e)
+        {
+            Assert.Fail($"Should not have thrown exception, but did: {e}");
+        }
+    }
+    
+    public static void DoesNotThrow<T>(Action testCode) where T : Exception
+    {
+        try
+        {
+            testCode.Invoke();
+        }
+        catch (T e)
+        {
+            Assert.Fail($"Should not have thrown exception of type {typeof(T).Name}, but did: {e}.");
+        }
+    }
+}
