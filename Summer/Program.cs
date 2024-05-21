a23970054e44dcc6b6a5aecc0c5682c15eba89a6
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-05-20 21:30:09 -0700

    Start ComponentsEngine on Main()

--- Summer/Program.cs
+++ Summer/Program.cs
@@ -1,9 +1,12 @@
-﻿namespace Summer;
+﻿using Summer.DependencyInjection;
+
+namespace Summer;
 
 internal class Program
 {
     static void Main(string[] args)
     {
+        ComponentsEngine.Start();
         Console.WriteLine("Hello, Summer!");
     }
 }
