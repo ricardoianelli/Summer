9c618fa02e679fcb3cb027990f52266a3e1d5b9f
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-06-05 23:57:46 -0700

    Ensure 1s delay between clock time changes

--- Summer/Components/Example/Clocks/Clock.cs
+++ Summer/Components/Example/Clocks/Clock.cs
@@ -20,7 +20,7 @@ public abstract class Clock : IComponent
 
     protected static string GetFormattedDateNowString()
     {
-        return DateTime.Now.ToString("HH:mm:ss");
+        return DateTime.Now.ToString("HH:mm:ss.fff");
     }
     
     private static async Task OnTimeChanged(ClockTime clockTime)
@@ -34,14 +34,13 @@ public abstract class Clock : IComponent
         while (true)
         {
             var time = DateTime.Now;
+            var msUntilNextSecond = 1000 - time.Millisecond + 100; // 100ms of margin
+            var nextSecDelay = Task.Delay(msUntilNextSecond);
             
-            // I don't want to wait here, I want to scream "Hey, stuff changed!" and not care about whoever hears it.
-            // If I wait, I would have to do extra calculations to ensure the 1s timeframe between runs.
-            // Think about it that way: If I lose 0.2s here waiting, and then wait for 1s, I actually had a 
-            // Difference of 1.2 seconds between OnTimeChanged events, instead of 1s. After 5s, I would be 1s late.
-            await OnTimeChanged(new ClockTime(time.Hour, time.Minute, time.Second)); 
-            
-            await Task.Delay(1000); //The clock goes 1s at a time, so let's wait for 1s.
+            // We don't want to await this because it will add an extra delay to our 1s timer.
+            // We can just fire and forget, it should be totally fine.
+            OnTimeChanged(new ClockTime(time.Hour, time.Minute, time.Second));
+            await nextSecDelay;
         }
     }
 
