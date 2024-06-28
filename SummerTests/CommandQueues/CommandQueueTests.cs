ac0f506c11b7f29c54aedb13f8d8152646fada47
Ricardo Macedo Ianelli <ricardoianelli@hotmail.com>
2024-06-27 19:35:32 -0700

    Try something with xunit

--- SummerTests/CommandQueues/CommandQueueTests.cs
+++ SummerTests/CommandQueues/CommandQueueTests.cs
@@ -5,82 +5,82 @@ namespace SummerTests.CommandQueues;
 
 public class CommandQueueTests
 {
-    private CommandQueue _queue;
-
-    public CommandQueueTests()
-    {
-        _queue = new CommandQueue(keepHistory: true);
-    }
-
     [Fact]
     public void CommandQueue_ShouldInitializeWithStoppedState()
     {
-        _queue.State.Should().Be(CommandQueueState.Stopped);
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.State.Should().Be(CommandQueueState.Stopped);
     }
 
     [Fact]
     public void SetState_GivenNewState_ShouldChangeState()
     {
-        _queue.SetState(CommandQueueState.Paused);
-        _queue.State.Should().Be(CommandQueueState.Paused);
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.SetState(CommandQueueState.Paused);
+        queue.State.Should().Be(CommandQueueState.Paused);
     }
 
     [Fact]
     public void SetPoolingDelay_GivenNewDelay_ShouldChangeDelay()
     {
+        CommandQueue queue = new CommandQueue(keepHistory: true);
         var newDelay = new Random().Next(0, 5000);
         
-        _queue.SetPoolingDelay(newDelay);
-        _queue.GetPoolingDelay().Should().Be(newDelay);
+        queue.SetPoolingDelay(newDelay);
+        queue.GetPoolingDelay().Should().Be(newDelay);
     }
 
     [Fact]
     public void Enqueue_GivenCommand_ShouldAddCommandToQueue()
     {
+        CommandQueue queue = new CommandQueue(keepHistory: true);
         var command = new MockCommand();
         
-        var lastCommand = _queue.GetNext();
-        _queue.Enqueue(command);
-        _queue.GetNext().Should().NotBe(lastCommand);
+        var lastCommand = queue.GetNext();
+        queue.Enqueue(command);
+        queue.GetNext().Should().NotBe(lastCommand);
     }
 
     [Fact]
     public void Start_GivenStoppedState_ShouldChangeStateToStarted()
     {
-        _queue.Start();
-        _queue.State.Should().Be(CommandQueueState.Started);
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.Start();
+        queue.State.Should().Be(CommandQueueState.Started);
     }
     
     [Fact]
     public void Start_GivenPausedState_ShouldChangeStateToStarted()
     {
-        _queue.SetState(CommandQueueState.Paused);
-        _queue.Start();
-        _queue.State.Should().Be(CommandQueueState.Started);
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.SetState(CommandQueueState.Paused);
+        queue.Start();
+        queue.State.Should().Be(CommandQueueState.Started);
     }
     
     [Fact]
     public async Task ProcessQueue_GivingStartAndStopMultipleTimes_ShouldOnlyRunOneInstanceOfMethod()
     {
-        _queue.Start();
-        _queue.Enqueue(new MockCommand());
-        _queue.Enqueue(new MockCommand());
-        _queue.Enqueue(new MockCommand());
-        await Task.Delay(_queue.GetPoolingDelay() /4);
-        _queue.Stop();
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.Start();
+        queue.Enqueue(new MockCommand());
+        queue.Enqueue(new MockCommand());
+        queue.Enqueue(new MockCommand());
+        await Task.Delay(queue.GetPoolingDelay() /4);
+        queue.Stop();
         
-        _queue.Start();
-        _queue.Enqueue(new AnonymousCommand(() => { }));
-        _queue.Enqueue(new AnonymousCommand(() => { }));
-        _queue.Enqueue(new AnonymousCommand(() => { }));
-        await Task.Delay(_queue.GetPoolingDelay() /4);
-        _queue.Stop();
+        queue.Start();
+        queue.Enqueue(new AnonymousCommand(() => { }));
+        queue.Enqueue(new AnonymousCommand(() => { }));
+        queue.Enqueue(new AnonymousCommand(() => { }));
+        await Task.Delay(queue.GetPoolingDelay() /4);
+        queue.Stop();
         
-        _queue.Start();
-        _queue.Enqueue(new MockCommand());
-        await Task.Delay(_queue.GetPoolingDelay() * 2);
+        queue.Start();
+        queue.Enqueue(new MockCommand());
+        await Task.Delay(queue.GetPoolingDelay() * 2);
 
-        var history = _queue.GetHistory();
+        var history = queue.GetHistory();
         history.Count.Should().Be(3);
         history[0].Should().BeOfType(typeof(MockCommand));
         history[1].Should().BeOfType(typeof(AnonymousCommand));
@@ -90,56 +90,61 @@ public class CommandQueueTests
     [Fact]
     public void Stop_GivenStartedState_ShouldChangeStateToStoppedAndClearQueue()
     {
-        _queue.Start();
-        _queue.Enqueue(new MockCommand());
-        _queue.Count().Should().Be(1);
-        _queue.Stop();
-        _queue.State.Should().Be(CommandQueueState.Stopped);
-        _queue.Count().Should().Be(0);
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.Start();
+        queue.Enqueue(new MockCommand());
+        queue.Count().Should().Be(1);
+        queue.Stop();
+        queue.State.Should().Be(CommandQueueState.Stopped);
+        queue.Count().Should().Be(0);
     }
 
     [Fact]
     public void Pause_GivenStartedState_ShouldChangeStateToPaused()
     {
-        _queue.Start();
-        _queue.Enqueue(new MockCommand());
-        _queue.Count().Should().Be(1);
-        _queue.Pause();
-        _queue.State.Should().Be(CommandQueueState.Paused);
-        _queue.Count().Should().Be(1);
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        queue.Start();
+        queue.Enqueue(new MockCommand());
+        queue.Count().Should().Be(1);
+        queue.Pause();
+        queue.State.Should().Be(CommandQueueState.Paused);
+        queue.Count().Should().Be(1);
     }
 
     [Fact]
     public async Task GetFromHistory_GivenValidIndex_ShouldReturnCommandFromHistory()
     {
+        CommandQueue queue = new CommandQueue(keepHistory: true);
         var firstCommand = new MockCommand();
         var secondCommand = new AnonymousCommand(() => { });
-        _queue.Enqueue(firstCommand);
-        _queue.Enqueue(secondCommand);
-        _queue.Start();
+        queue.Enqueue(firstCommand);
+        queue.Enqueue(secondCommand);
+        queue.Start();
 
-        await Task.Delay(_queue.GetPoolingDelay()*10);
+        await Task.Delay(queue.GetPoolingDelay()*10);
         
-        _queue.GetFromHistory(0).Should().Be(firstCommand);
-        _queue.GetFromHistory(1).Should().Be(secondCommand);
+        queue.GetFromHistory(0).Should().Be(firstCommand);
+        queue.GetFromHistory(1).Should().Be(secondCommand);
     }
 
     [Fact]
     public void GetFromHistory_GivenInvalidIndex_ShouldThrowException()
     {
-        Assert.Throws<Exception>(() => _queue.GetFromHistory(0));
+        CommandQueue queue = new CommandQueue(keepHistory: true);
+        Assert.Throws<Exception>(() => queue.GetFromHistory(0));
     }
 
     [Fact]
     public async Task GetHistory_GivenExecutedCommands_ShouldReturnAllCommandsFromHistory()
     {
+        CommandQueue queue = new CommandQueue(keepHistory: true);
         var command = new MockCommand();
-        _queue.Enqueue(command);
-        _queue.Start();
+        queue.Enqueue(command);
+        queue.Start();
 
-        await Task.Delay(_queue.GetPoolingDelay()*2);
+        await Task.Delay(queue.GetPoolingDelay()*2);
 
-        var history = _queue.GetHistory();
+        var history = queue.GetHistory();
         history.Count.Should().Be(1);
         history[0].Should().Be(command);
     }
@@ -147,11 +152,12 @@ public class CommandQueueTests
     [Fact]
     public async Task CommandExecution_GivenStartedQueue_ShouldExecuteCommandsInQueue()
     {
+        CommandQueue queue = new CommandQueue(keepHistory: true);
         var command = new MockCommand();
-        _queue.Enqueue(command);
-        _queue.Start();
+        queue.Enqueue(command);
+        queue.Start();
 
-        await Task.Delay(_queue.GetPoolingDelay()*2);
+        await Task.Delay(queue.GetPoolingDelay()*2);
 
         command.Executed.Should().BeTrue();
     }
